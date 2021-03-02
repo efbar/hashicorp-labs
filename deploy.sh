1f4c8819710d1d71a5d885554ccef0f6d9d83e0a
efbar <17676301+efbar@users.noreply.github.com>
2021-03-02 18:41:00 +0100

    fix output message

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-# set -e
+set -e
 
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# echo -e "\033[32mProvisioning VM..\033[0m"
-# export VAGRANT_CWD="${DIR}/vagrant"
-# vagrant validate && \
-# vagrant up
+echo -e "\033[32mProvisioning VM..\033[0m"
+export VAGRANT_CWD="${DIR}/vagrant"
+vagrant validate && \
+vagrant up
 
-# echo -e "\033[32mDeploying workload..\033[0m"
-# cd ${DIR}/terraform
-# terraform init && terraform plan && terraform apply -auto-approve && \
+echo -e "\033[32mDeploying workload..\033[0m"
+cd ${DIR}/terraform
+terraform init && terraform plan && terraform apply -auto-approve && \
 echo -e "\033[32m 
  Done!
 
