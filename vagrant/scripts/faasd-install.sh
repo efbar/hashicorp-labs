7004cbc3eabb4cb4ddd89ed7ebc1e87fb844cc51
efbar <17676301+efbar@users.noreply.github.com>
2021-03-06 12:57:47 +0100

    not needed anymore

--- vagrant/scripts/faasd-install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-set -e
-
-[[ -f faasd ]] || wget -q https://github.com/openfaas/faasd/releases/download/${FAASD_VERSION}/faasd && \
-sudo mv faasd /usr/local/bin/faasd && chmod +x /usr/local/bin/faasd
