88792f8a0844bd3db9e894898ed5a30ef7c93441
efbar <17676301+efbar@users.noreply.github.com>
2021-03-06 12:52:15 +0100

    better cleaning

--- clean.sh
+++ clean.sh
@@ -5,7 +5,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo -e "\033[32mCleaning VM..\033[0m"
+
 export VAGRANT_CWD="${DIR}/vagrant"
 vagrant destroy -f
+[ -z terraform/terraform.tfstate ] && rm terraform/terraform.tfstate 
+[ -z terraform/terraform.tfstate.backup ] && rm terraform/terraform.tfstate.backup
+[ -d terraform/.terraform ] && rm -r terraform/.terraform
+[ -d vagrant/.vagrant ] && rm -r vagrant/.vagrant
 
 echo -e "\033[32mDone."
