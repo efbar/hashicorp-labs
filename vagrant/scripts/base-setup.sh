982f9b03c693830047f9632305bdd2e48a616c58
efbar <17676301+efbar@users.noreply.github.com>
2021-04-23 16:21:59 +0200

    cleaning

--- vagrant/scripts/base-setup.sh
+++ vagrant/scripts/base-setup.sh
@@ -7,13 +7,10 @@ sudo timedatectl set-timezone UTC
 
 echo "RHEL/CentOS system detected"
 echo "Performing updates and installing prerequisites"
-sudo yum-config-manager --enable rhui-REGION-rhel-server-releases-optional
-sudo yum-config-manager --enable rhui-REGION-rhel-server-supplementary
-sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
 sudo yum -y check-update
 
 echo "Install some packages"
-sudo yum install ntp git wget unzip jq dnsmasq openjdk-8-jdk java-1.8.0-openjdk bind-utils tcpdump nc -y
+sudo yum install ntp git wget unzip jq dnsmasq java-1.8.0-openjdk bind-utils tcpdump nc -y
 
 echo "Start NTP server"
 sudo systemctl start ntpd.service
